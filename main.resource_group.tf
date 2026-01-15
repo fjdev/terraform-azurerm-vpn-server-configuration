86f0d26a1207d9d9debbaade8b9b250fea3fbb39
Florian de Vries <custadmin.floriadv@cegekagroup.onmicrosoft.com>
2026-01-15 14:27:38 +0100

    Reintroduce resource group definition in main.tf and set default for policy_groups variable

--- main.resource_group.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  count = var.deploy_resource_group ? 1 : 0
-
-  name       = var.resource_group_name
-  location   = var.location
-  managed_by = var.managed_by
-  tags       = try(var.tags.resource_group, null)
-}
